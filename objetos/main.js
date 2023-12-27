55b8d8985122cde35e4b61ea0d5f9922024e4e55
mclaradev <mclaradev@gmail.com>
2023-12-27 12:22:59 -0300

    estudei objetos e conclui um exercicio

---
+++ objetos/main.js
@@ -0,0 +1,10 @@
+const pessoa = {}
+pessoa.nome  = "flora"
+pessoa.idade = 20
+console.log(pessoa)
+
+pessoa.colegas = ["steph", "joão", "adelia"]
+pessoa.enderco = {
+    rua: "logo ali",
+    numero: 333,
+    bairro: "centro"}
