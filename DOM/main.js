8fbad93886e2f0c22969c3bc0d5a2f50199b54da
mclaradev <mclaradev@gmail.com>
2024-01-11 14:25:13 -0300

    primeiro commit DOM

---
+++ DOM/main.js
@@ -0,0 +1,19 @@
+function onMouseEnter() {
+    console.log("O mouse entrou no elemento?")
+}
+
+function onChange() {
+    console.log("Você mudou algo no elemento?")
+}
+
+function onFocus() {
+    console.log("Você focou no elemento?")
+}
+
+function onBlur() {
+    console.log("Você desfocou o elemento.")
+}
+
+function onClick() {
+    console.log("Uau, você clicou no botão. Parabéns.")
+}
