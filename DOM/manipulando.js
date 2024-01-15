8c9cb7a97e4272bd80d7d0b6f1b3a3ea96f40d4b
mclaradev <mclaradev@gmail.com>
2024-01-15 12:49:41 -0300

    praticando a manipulação do DOM

---
+++ DOM/manipulando.js
@@ -0,0 +1,50 @@
+function addContact() {
+    const contactSection = document.getElementById('contact-list')
+
+    const h3 = document.createElement('h3')
+    h3.innerText = 'Contato'
+
+    const ul = document.createElement('ul')
+
+    const nameLi = document.createElement('li')
+    nameLi.innerText = "Nome: "
+
+    const nameInput = document.createElement('input')
+    nameInput.type = 'text'
+    nameInput.name = 'fullname'
+
+    nameLi.appendChild(nameInput)
+    ul.appendChild(nameLi)
+    ul.append(document.createElement('br'))
+
+    const phoneLi = document.createElement('li')
+    phoneLi.innerText = "Telefone: "
+    const phoneInput = document.createElement('input')
+    phoneInput.type = 'number'
+    phoneInput.name = 'phone'
+    phoneLi.appendChild(phoneInput)
+    ul.appendChild(phoneLi)
+    ul.append(document.createElement('br'))
+
+    const adressLi = document.createElement('li')
+    adressLi.innerHTML = '<label for="adress">Endereço: </label>'
+    const adressInput = document.createElement('input')
+    adressInput.type = 'text'
+    adressInput.name = 'adress'
+    adressId = 'adress'
+    adressLi.appendChild(adressInput)
+    ul.appendChild(adressLi)
+    ul.append(document.createElement('br'))
+
+    contactSection.append(h3, ul)
+}
+
+function removeContact() {
+    const contactSection = document.getElementById('contact-list')
+
+    const titles = document.getElementsByTagName('h3')
+    const contacts = document.getElementsByTagName('ul')
+
+    contactSection.removeChild(titles[titles.length - 1])
+    contactSection.removeChild(contacts[contacts.length - 1])
+}
