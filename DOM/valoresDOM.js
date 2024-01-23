7a117f676554083c284c6bdb3b69782e0fff7af1
flora <mclaradev@gmail.com>
2024-01-23 18:36:36 -0300

    enviando valores do DOM para o js - teste 2

--- DOM/valoresDOM.js
+++ DOM/valoresDOM.js
@@ -1,3 +1,11 @@
 function register(element) {
-    console.log(element)
+    const username = element.children.username.value
+    const password = element.children.password.value
+    const passwordConfirmation = element.children.passwordConfirmation.value
+
+    if (password === passwordConfirmation) {
+        alert(`Parabéns ${username} por se cadastrar!`)
+    } else {
+        alert(`ERROR: esqueceu a senha ${username}? Preste mais atenção.`)
+    }
 }
