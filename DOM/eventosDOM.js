6fefbb90a6662a8de3c3dda790fdc81960d6766a
flora <mclaradev@gmail.com>
2024-01-23 19:25:15 -0300

    testando os eventos no javascript

---
+++ DOM/eventosDOM.js
@@ -0,0 +1,21 @@
+function register(ev) {
+    console.log(ev)
+    const sectionElement = ev.currentTarget.parentNode
+    const username = sectionElement.children.username.value
+    const password = sectionElement.children.password.value
+    const passwordConfirmation = sectionElement.children.passwordConfirmation.value
+
+    if (password === passwordConfirmation) {
+        alert(`Usuário ${username} registrado!`)
+    } else {
+        alert(`ERROR: esqueceu a senha ${username}?`)
+    }
+}
+
+const button = document.getElementById('register-button')
+
+button.addEventListener('click', register)
+
+function removerEvento() {
+    button.removeEventListener('click', register)
+}
