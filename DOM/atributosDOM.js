6f6e9f7add641a28c984df9c2340f83eefd5fdbe
flora <mclaradev@gmail.com>
2024-01-29 10:09:36 -0300

    finalização de "testes" no js.

--- DOM/atributosDOM.js
+++ DOM/atributosDOM.js
@@ -7,3 +7,21 @@ document.getElementById('value').addEventListener('click', function () {
     console.log(input.getAttribute('value'))
 })
 
+document.getElementById('type').addEventListener('click', function () {
+    // input.type = input.type !== 'date' ? 'date' : 'text'
+    input.setAttribute('type', 'radio')
+})
+
+document.getElementById('placeholder').addEventListener('click', function () {
+    input.placeholder = 'Digite algo...'
+})
+
+document.getElementById('disable').addEventListener('click', function () {
+    input.setAttribute('disable', !input.disable)
+})
+
+document.getElementById('data').addEventListener('click', function () {
+    const data = input.dataset.something
+    console.log(`O valor do atributo data-something é: ${data}`)
+    input.dataset.something = 'Algum outro valor.'
+})
