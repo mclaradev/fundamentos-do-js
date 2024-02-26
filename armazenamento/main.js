8ece1522279c02ecf9d1600a6ff73bc157357a52
flora <mclaradev@gmail.com>
2024-02-26 11:43:20 -0300

    segunda parte do código de js

--- armazenamento/main.js
+++ armazenamento/main.js
@@ -8,3 +8,14 @@ document.getElementById('readSession').addEventListener('click', function() {
     const info = sessionStorage.getItem('info')
     alert(`A informação salva é: "${info}".`)
 })
+
+document.getElementById('localBtn').addEventListener('click', function () {
+    const input = document.getElementById('local')
+    localStorage.setItem('text', input.value)
+    input.value = ''
+})
+
+document.getElementById('readLocal').addEventListener('click', function() {
+    const t = localStorage.getItem('text')
+    alert(`A informação salva é: "${t}".`)
+})
